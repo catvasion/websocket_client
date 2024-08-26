0ba8bcbf0e1e1104b5e9c86c858c0a7029dbe48f
Emily Dawson <lemilydawson@gmail.com>
2024-08-26 18:00:09 +0100

    [feature/02] add logic to display errors

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,14 @@
-const Error = () => {
-	return <div>Error</div>
+import { StatResponse } from '../types'
+const Error = ({ error }: { error: StatResponse['error'] }) => {
+	return (
+		<main className='flex items-center justify-center bg-purple-200 min-h-screen p-10'>
+			<div className='flex flex-col'>
+				<h1 className='title'>{error?.code} Error:</h1>
+				<p className='subtitle'>Oops something went wrong.</p>
+				<p className='body !text-red-500'>{error?.message}.</p>
+			</div>
+		</main>
+	)
 }
 
 export default Error
