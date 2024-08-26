7a9be4c5008ae6717ac1d9fa5754a522652699d0
Emily Dawson <lemilydawson@gmail.com>
2024-08-26 13:05:01 +0100

    setup project structure and configuration

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<main className='body flex flex-col items-center justify-center bg-purple-200 min-h-screen p-10'>
+			Websocket App
+		</main>
+	)
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Card } from './Card'
+export { default as Tab } from './Tab'
+export { default as Loading } from './Loading'
+export { default as Error } from './Error'
